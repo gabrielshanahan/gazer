0baa3ba2609255c49fd21c3583d9e476317e7e0d
Gabriel Shanahan <GabrielShanahan@gmail.com>
2020-04-06 18:05:38 +0200

    Added jpa/mysql dependencies and set up basic docker compose

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Gazer
 
 ## Setup
+```bash
+export MYSQL_ROOT_PASSWORD=<...>
+docker compose up
+```
+
+## Optional config
+MYSQL_DATABASE (default=gazer)
--- build.gradle.kts
+++ build.gradle.kts
@@ -40,6 +40,9 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    runtimeOnly("mysql:mysql-connector-java")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
 	}
