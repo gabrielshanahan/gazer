8eadc8cb011ff23d3f126123280d88fc270e05ff
Gabriel Shanahan <GabrielShanahan@gmail.com>
2020-04-09 14:48:45 +0200

    Cleaned up multimodule refactor

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,36 +3,95 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     val kotlinVersion = "1.3.71"
 
-	kotlin("jvm") version kotlinVersion apply false
-	kotlin("plugin.spring") version kotlinVersion apply false
-    kotlin("plugin.allopen") version kotlinVersion apply false
-    kotlin("plugin.noarg") version kotlinVersion apply false
-    kotlin("plugin.jpa") version kotlinVersion apply false
-
-	id("org.springframework.boot") version "2.3.0.M4" apply false
-	id("io.spring.dependency-management") version "1.0.9.RELEASE" apply false
+	kotlin("jvm") version kotlinVersion
 
 	`java-library`
 	jacoco
 
-	id("org.jmailen.kotlinter") version "2.3.2" apply false
-	id("io.gitlab.arturbosch.detekt") version "1.7.4" apply false
-	id("org.jetbrains.dokka") version "0.10.1" apply false
+	id("org.jmailen.kotlinter") version "2.3.2"
+	id("io.gitlab.arturbosch.detekt") version "1.7.4"
+	id("org.jetbrains.dokka") version "0.10.1"
+
+    // Spring specific plugins
+    kotlin("plugin.spring") version kotlinVersion apply false
+    kotlin("plugin.allopen") version kotlinVersion apply false
+    kotlin("plugin.noarg") version kotlinVersion apply false
+    kotlin("plugin.jpa") version kotlinVersion apply false
+
+    id("org.springframework.boot") version "2.3.0.M4" apply false
+    id("io.spring.dependency-management") version "1.0.9.RELEASE" apply false
+
 }
 
-group = "io.github.gabrielshanahan"
-version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_1_8
+allprojects {
 
-val developmentOnly by configurations.creating
-configurations {
-	runtimeClasspath {
-		extendsFrom(developmentOnly)
-	}
+    apply {
+        plugin("java-library")
+    }
+
+    group = "io.github.gabrielshanahan"
+    version = "0.0.1-SNAPSHOT"
+    java.sourceCompatibility = JavaVersion.VERSION_1_8
+
+    repositories {
+        mavenCentral()
+        maven { url = uri("https://repo.spring.io/milestone") }
+        jcenter()
+    }
 }
 
-repositories {
-	mavenCentral()
-    maven { url = uri("https://repo.spring.io/milestone") }
-    jcenter()
+subprojects {
+
+    apply {
+
+        plugin("org.jetbrains.kotlin.jvm")
+        plugin("jacoco")
+        plugin("org.jmailen.kotlinter")
+        plugin("io.gitlab.arturbosch.detekt")
+        plugin("org.jetbrains.dokka")
+
+    }
+
+    dependencies {
+        implementation(platform(kotlin("bom")))
+        implementation(kotlin("reflect"))
+        implementation(kotlin("stdlib-jdk8"))
+
+        detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.7.4")
+    }
+
+    tasks {
+        test {
+            useJUnitPlatform()
+            // Always run ktlint, detekt and jacoco when running tests
+            finalizedBy(jacocoTestReport, lintKotlin, detekt)
+
+            testLogging {
+                // Make sure output from
+                // standard out or error is shown
+                // in Gradle output.
+                showStandardStreams = true
+            }
+        }
+
+        dokka {
+            outputFormat = "html"
+            configuration {
+                includeNonPublic = true
+                reportUndocumented = true
+            }
+        }
+
+        withType<KotlinCompile> {
+            kotlinOptions {
+                freeCompilerArgs = listOf("-Xjsr305=strict")
+                jvmTarget = "1.8"
+            }
+        }
+    }
+
+    detekt {
+        baseline = file("${rootProject.projectDir}/config/detekt/baseline.xml")
+        parallel = true // Builds the AST in parallel. Rules are always executed in parallel.
+    }
 }
--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -1,22 +1,39 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
 
-    kotlin("jvm")
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
+
     kotlin("plugin.spring")
     kotlin("plugin.allopen")
     kotlin("plugin.noarg")
     kotlin("plugin.jpa")
+}
 
-	id("org.springframework.boot")
-	id("io.spring.dependency-management")
+val developmentOnly by configurations.creating
+configurations {
+    runtimeClasspath {
+        extendsFrom(developmentOnly)
+    }
+}
 
-	`java-library`
-	jacoco
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
-	id("org.jmailen.kotlinter")
-	id("io.gitlab.arturbosch.detekt")
-	id("org.jetbrains.dokka")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    runtimeOnly("mysql:mysql-connector-java")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+    }
+
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+}
+
+tasks {
+    bootJar {
+        layered()
+    }
 }
 
 allOpen {
@@ -24,78 +41,3 @@ allOpen {
     annotation("javax.persistence.MappedSuperclass")
     annotation("javax.persistence.Embeddable")
 }
-
-group = "io.github.gabrielshanahan"
-version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_1_8
-
-val developmentOnly by configurations.creating
-configurations {
-	runtimeClasspath {
-		extendsFrom(developmentOnly)
-	}
-}
-
-repositories {
-	mavenCentral()
-    maven { url = uri("https://repo.spring.io/milestone") }
-    jcenter()
-}
-
-dependencies {
-	implementation(platform(kotlin("bom")))
-	implementation(kotlin("reflect"))
-	implementation(kotlin("stdlib-jdk8"))
-
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    runtimeOnly("mysql:mysql-connector-java")
-
-	testImplementation("org.springframework.boot:spring-boot-starter-test") {
-		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
-	}
-
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
-    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.7.4")
-}
-tasks {
-    test {
-        useJUnitPlatform()
-        // Always run ktlint, detekt and jacoco when running tests
-        finalizedBy(jacocoTestReport, lintKotlin, detekt)
-
-        testLogging {
-            // Make sure output from
-            // standard out or error is shown
-            // in Gradle output.
-            showStandardStreams = true
-        }
-    }
-
-    dokka {
-        outputFormat = "html"
-        configuration {
-            includeNonPublic = true
-            reportUndocumented = true
-        }
-    }
-
-    withType<KotlinCompile> {
-        kotlinOptions {
-            freeCompilerArgs = listOf("-Xjsr305=strict")
-            jvmTarget = "1.8"
-        }
-    }
-
-    bootJar {
-        layered()
-    }
-}
-
-detekt {
-    baseline = file("${rootProject.projectDir}/config/detekt/baseline.xml")
-    parallel = true // Builds the AST in parallel. Rules are always executed in parallel.
-}
-
