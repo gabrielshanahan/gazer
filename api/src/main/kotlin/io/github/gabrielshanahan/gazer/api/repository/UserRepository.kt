df9aa0605a4f1ec719bf24e64044f712447798eb
Gabriel Shanahan <GabrielShanahan@gmail.com>
2020-04-13 22:54:23 +0200

    Made username unique, prevented UserRepo from exporting

--- api/src/main/kotlin/io/github/gabrielshanahan/gazer/api/repository/UserRepository.kt
+++ api/src/main/kotlin/io/github/gabrielshanahan/gazer/api/repository/UserRepository.kt
@@ -3,5 +3,7 @@ package io.github.gabrielshanahan.gazer.api.repository
 import io.github.gabrielshanahan.gazer.data.model.User
 import java.util.*
 import org.springframework.data.repository.CrudRepository
+import org.springframework.data.rest.core.annotation.RepositoryRestResource
 
+@RepositoryRestResource(exported = false)
 interface UserRepository : CrudRepository<User, UUID>
--- data/src/main/kotlin/io/github/gabrielshanahan/gazer/data/model/User.kt
+++ data/src/main/kotlin/io/github/gabrielshanahan/gazer/data/model/User.kt
@@ -1,12 +1,16 @@
 package io.github.gabrielshanahan.gazer.data.model
 
 import java.util.UUID
+import javax.persistence.Column
 import javax.persistence.Entity
 
 @Entity
 class User(
     id: UUID? = null,
+
+    @Column(unique=true)
     val username: String,
+
     val email: String,
     val token: String
 ) : AbstractEntity(id)
