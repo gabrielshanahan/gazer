e6c0a751f7fde50d29b5aaa2d52672b11e6a93a0
Gabriel Shanahan <GabrielShanahan@gmail.com>
2020-04-24 20:19:18 +0200

    Fixed gazer tests failing when being run all at once by making the H2 Bean only available with the dev profile

--- data/src/main/kotlin/io/github/gabrielshanahan/gazer/data/DataConfiguration.kt
+++ data/src/main/kotlin/io/github/gabrielshanahan/gazer/data/DataConfiguration.kt
@@ -1,11 +1,12 @@
 package io.github.gabrielshanahan.gazer.data
 
+import java.sql.SQLException
 import org.h2.tools.Server
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
-import java.sql.SQLException
 
 @Configuration
 @EnableJpaRepositories
@@ -13,6 +14,7 @@ import java.sql.SQLException
 class DataConfiguration {
 
     @Bean(initMethod = "start", destroyMethod = "stop")
+    @Profile("dev")
     @Throws(SQLException::class)
     fun h2Server(): Server = Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092")
 }
